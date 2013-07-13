5c9b6c0097af3cc3196364682d9d3517b596d908
jjchen <junjunc@princeton.edu>
2013-07-13 00:52:18 -0700

    somethign

--- geotour/views.py
+++ geotour/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from geotour.models import Place
+from geotour.models import Tour
 from django.template.defaulttags import csrf_token
 
 def home(request):
@@ -12,11 +13,14 @@ def home(request):
 		# destination = request.POST['destination']
 		# tour = Tour('name': request.POST['name'])
 		# tour.save()
-		return HttpResponseRedirect('/results')
+		# tourId = tour.id
+		tourId = 1
+		return HttpResponseRedirect('/results/'+str(tourId))
 	return render(request, 'home.html', {})
 
-def results(request):
-	places = Place.objects.all()
+def results(request, tourId):
+	tour = Tour.objects.get(id = tourId)
+	places = Place.objects.filter(tour = tour)
 	return render(request, 'results.html', {
 		'places': places
 		})
