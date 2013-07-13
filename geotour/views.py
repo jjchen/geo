9b52c2c9ab6cef12a51522e529f09de5eeb7dc14
Jiexi Cao <jcao@hmc.edu>
2013-07-12 23:03:50 -0700

    Modified python files to include initial pages

--- geotour/views.py
+++ geotour/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+
+def home(request):
+    t = get_template('home.html')
+    html = t.render(Context({}))
+    return HttpResponse(html)
