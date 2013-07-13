3383d28c110e1e6f2e7a4dba5419f6b3d0615de3
jjchen <junjunc@princeton.edu>
2013-07-12 23:01:56 -0700

    ...

--- geotour/views.py
+++ geotour/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+# Create your views here
+from geotours.models import Tour
+from geotours.models import Area
+from geotours.models import Place
+from geotours.models import Transport
+
+class SearchForm(forms.Form):
+
+
+def home(request):
+	# if request.method == 'POST':
+	# 	form = SearchForm(request.POST)
+	# 	if form.is_valid():
+	# 		data = form.cleaned_data
+	# 		tour = Tour()
+	# 		tour.save()
+
+	# return render(request, 'home.html', {
+	# 	'form': form
+	# 	})
+	return render(request, 'home.html')
+
+def results(request):
+	# places = Places.objects.filter()
+
+	# return render(request, 'results.html', {
+
+	# 	})
+	return render(request, 'results.html')
