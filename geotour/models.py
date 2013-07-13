b19805505ef9354419492410e29ea8f3775b81d4
jjchen <junjunc@princeton.edu>
2013-07-12 22:28:31 -0700

    models

--- geotour/models.py
+++ geotour/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
-class Place(models.Model):
+class Tour(models.Model):
 	name = models.CharField(max_length=200)
+	fromAddress = models.CharField(max_length=200)
+	destination = models.CharField(max_length=200)
+	returnAddress = models.CharField(max_length=200)
+	startTime = models.DateTimeField(default=datetime.date.today())
+	endTime = models.DateTimeField(default=datetime.date.today())
+	transport = models.ManyToManyField(Transport)
+	areas =  models.ManyToManyField(Area)	
+
+class Transport(models.Model):
+    transportType = models.CharField(max_length=200)
+
+class Area(models.Model):
+    interestType = models.CharField(max_length=200)
+
+class Place(models.Model):
+	tour = models.ForeignKey(Tour)
+	name = models.CharField(max_length=200)
+	time = models.DateTimeField(default=datetime.date.today())
+	address = models.CharField(max_length=200)
+	details =  models.CharField(max_length=200)
+
