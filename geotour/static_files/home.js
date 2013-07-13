30be389cf355b007069623fcf1c717b1fe0b5dd9
Jiexi Cao <jcao@hmc.edu>
2013-07-13 02:05:46 -0700

    Added datepicker library, but it's not quite working

---
+++ geotour/static_files/home.js
@@ -0,0 +1,12 @@
+
+
+var addEndField = function() {
+    $("#endField").show();
+    $("#showEndFieldText").hide();
+    $("#showEndField").css("padding-top", "10px");
+};
+
+
+$(document).ready(function() {
+    $("#dateInput").datepicker();
+});
