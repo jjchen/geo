05b810e30800a561c9358fd176f8471e42f213eb
jjchen <junjunc@princeton.edu>
2013-07-13 01:36:16 -0700

    add?

---
+++ geotour/urls.py
@@ -0,0 +1,9 @@
+from geotour import views
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+	url(r'^$', views.home, name='home'),
+	url(r'^results/(?P<tourId>\w+)/$', views.results, name='results'),
+	url(r'^test/', views.test, name='test'),
+)
