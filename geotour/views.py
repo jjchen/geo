133eb9c2e39e38c21f9bf0daf05cd57c15229179
jjchen <junjunc@princeton.edu>
2013-07-12 23:46:31 -0700

    merge

--- geotour/views.py
+++ geotour/views.py
@@ -1,8 +1,9 @@
 
-
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
+from geotour.models import Place
 
 def home(request):
     t = get_template('home.html')
@@ -27,17 +28,10 @@ def home(request):
 # 	return render(request, 'home.html')
 
 def results(request):
-	# places = Places.objects.filter()
-
-	# return render(request, 'results.html', {
-
-	# # 	})
-	# return render(request, 'results.html')
-
-
-    t = get_template('results.html')
-    html = t.render(Context({}))
-    return HttpResponse(html)
+	places = Place.objects.all()
+	return render(request, 'results.html', {
+		'places': places
+		})
 
 def test(request):
 	t = get_template('test.html')
