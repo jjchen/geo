85ffb954e463d5d63dff02d04cbea06160d7de0e
root <root@jisha-dell.(none)>
2013-07-13 02:05:45 -0400

    synced datatbase

--- geotour/models.py
+++ geotour/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django.utils import timezone
 import datetime
-
-class Tour(models.Model):
-	name = models.CharField(max_length=200)
-	fromAddress = models.CharField(max_length=200)
-	destination = models.CharField(max_length=200)
-	returnAddress = models.CharField(max_length=200)
-	startTime = models.DateTimeField(default=datetime.date.today())
-	endTime = models.DateTimeField(default=datetime.date.today())
-	transport = models.ManyToManyField(Transport)
-	areas =  models.ManyToManyField(Area)	
 
 class Transport(models.Model):
     transportType = models.CharField(max_length=200)
 
 class Area(models.Model):
     interestType = models.CharField(max_length=200)
+
+class Tour(models.Model):
+	name = models.CharField(max_length=200)
+	fromAddress = models.CharField(max_length=200)
+	destination = models.CharField(max_length=200)
+	returnAddress = models.CharField(max_length=200)
+	startTime = models.DateTimeField(default=timezone.now())
+	endTime = models.DateTimeField(default=datetime.date.today())
+	transport = models.ManyToManyField(Transport)
+	areas =  models.ManyToManyField(Area)	
 
 class Place(models.Model):
 	tour = models.ForeignKey(Tour)
